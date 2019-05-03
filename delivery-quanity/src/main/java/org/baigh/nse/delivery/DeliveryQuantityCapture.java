a1daa9838d5afdd8bc64283859ba551da0cfac68
yasarbaigh <baigh.yasar@gmail.com>
2019-05-03 18:51:49 +0530

    delivery quantity

---
+++ delivery-quanity/src/main/java/org/baigh/nse/delivery/DeliveryQuantityCapture.java
@@ -0,0 +1,113 @@
+package org.baigh.nse.delivery;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.Select;
+
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.coordinates.WebDriverCoordsProvider;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+
+public class DeliveryQuantityCapture {
+	public static void main(String[] args) throws Exception {
+
+		System.setProperty("webdriver.gecko.driver", "/opt/jars/sel/geckodriver");
+
+		final FirefoxOptions options = new FirefoxOptions();
+		options.addArguments("--start-maximized", "--start-fullscreen");
+
+		final WebDriver driver = new FirefoxDriver(options);
+
+		driver.get("https://www.nseindia.com/products/content/equities/equities/eq_security.htm");
+		System.out.println(System.currentTimeMillis());
+		final String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+
+		new File(date).mkdirs();
+		final String scripts = "RELIANCE,VOLTAS,BHEL,BEL,BATAINDIA";
+		for (final String item : scripts.split(",")) {
+			getDQ(driver, date, item);
+		}
+
+		driver.close();
+		driver.quit();
+
+	}
+
+	private static void getDQ(WebDriver driver, String directoryValue, String script) throws Exception {
+
+		Thread.sleep(5000l);
+
+		driver.findElement(By.id("symbol")).clear();
+		driver.findElement(By.id("symbol")).sendKeys(script);
+		final Select dropdown = new Select(driver.findElement(By.id("dateRange")));
+
+		// dropdown.selectByValue("15days");
+		dropdown.selectByValue("1month");
+
+		driver.findElement(By.className("getdata-button")).click();
+
+		Thread.sleep(5000l);
+
+		takeFullPageScreenShotAsByte(driver, directoryValue, script);
+	}
+
+	private static void takeFullPageScreenShotAsByte(WebDriver webDriver, String directoryValue, String script)
+			throws Exception {
+		final Screenshot fpScreenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000))
+				.takeScreenshot(webDriver);
+
+		final BufferedImage originalImage = fpScreenshot.getImage();
+
+		ImageIO.write(originalImage, "PNG", new File(directoryValue + File.separator + script + ".png"));
+
+	}
+
+	private static byte[] takeFullPageScreenShotAsByteArray(WebDriver webDriver, String directoryValue, String script)
+			throws Exception {
+		final Screenshot fpScreenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000))
+				.takeScreenshot(webDriver);
+
+		final BufferedImage originalImage = fpScreenshot.getImage();
+
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			ImageIO.write(originalImage, "png", baos);
+			baos.flush();
+			return baos.toByteArray();
+		}
+
+	}
+
+	private void elementScreenCapture(WebDriver driver, String directoryValue, String script) throws IOException {
+
+		final String fileName = directoryValue + File.separator + script + ".png";
+		final WebElement webElement = driver.findElement(By.className("archives"));
+
+		final Screenshot screenshot = new AShot().coordsProvider(new WebDriverCoordsProvider()).takeScreenshot(driver,
+				webElement);
+		ImageIO.write(screenshot.getImage(), "PNG", new File(fileName));
+	}
+
+	private void normalScreenCapture(WebDriver driver, String directoryValue, String script) throws Exception {
+
+		final File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(scrFile, new File(directoryValue + File.separator + script + ".png"));
+
+	}
+
+}
